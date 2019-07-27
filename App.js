0c0e0db73f6f8c7ea1bed6b66398e18d969b19a7
Seri-Lis <seri@lifeis.style>
2019-07-27 16:20:57 +0900

    feat:fetch github API

--- App.js
+++ App.js
@@ -5,13 +5,25 @@ import {
   ScrollView,
   View,
   Text,
+  TouchableOpacity
 } from 'react-native';
 
 export default class App extends React.Component {
+  onPressFetch() {
+    // https://api.github.com/search/repositories?q=react
+    fetch('https://api.github.com/search/repositories?q=react')
+      .then(response => response.json())
+      .then(({ items }) => console.log(items))
+
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.wrapper}>
         <View style={styles.container}>
+          <TouchableOpacity onPress={() => this.onPressFetch()}>
+            <Text>Fetch</Text>
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     );
